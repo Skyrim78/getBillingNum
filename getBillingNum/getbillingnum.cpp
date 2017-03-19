395e658513f9ae5c093b87ef9ded822e9d2519b7
Ev Shevchenko <2058931@gmail.com>
2017-03-19 15:25:24 +0200

    v0.1

--- getBillingNum/getbillingnum.cpp
+++ getBillingNum/getbillingnum.cpp
@@ -6,12 +6,19 @@ getBillingNum::getBillingNum(QWidget *parent) :
     ui(new Ui::getBillingNum)
 {
     ui->setupUi(this);
-    ui->progressBar->hide();
-    ui->progressBar_num->hide();
 
-    folder = "/home/ev/Загрузки/testBilling";
 
-    get_data();
+    ui->groupBox_message->hide();
+    timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), ui->groupBox_message, SLOT(hide()));
+
+    ui->progressBar_file->hide();
+    ui->progressBar_data->hide();
+
+    connect(ui->toolButton_folder, SIGNAL(clicked(bool)), this, SLOT(selectFolder()));
+    connect(ui->pushButton_getData, SIGNAL(clicked(bool)), this, SLOT(selectFileName()));
+
+    readSetting();
 }
 
 getBillingNum::~getBillingNum()
@@ -19,81 +26,257 @@ getBillingNum::~getBillingNum()
     delete ui;
 }
 
-void getBillingNum::get_data()
+void getBillingNum::closeEvent(QCloseEvent *event)
 {
-    for (int r = ui->tableWidget->rowCount() - 1; r >= 0; r--){
-        ui->tableWidget->removeRow(r);
+    if (event->isAccepted()){
+        writeSetting();
     }
+}
 
-    ui->progressBar->setValue(0);
-    ui->progressBar->setVisible(true);
-    ui->progressBar_num->setValue(0);
-    ui->progressBar_num->setVisible(true);
+void getBillingNum::writeSetting()
+{
+    QSettings sett("setting.ini", QSettings::IniFormat);
+    sett.setValue("size", size());
+    sett.setValue("pos", pos());
+    sett.setValue("lastPath", folder.absolutePath());
+}
 
-    QStringList nameFilter;
-    nameFilter << "*.fp3";
+void getBillingNum::readSetting()
+{
+    QSettings sett("setting.ini", QSettings::IniFormat);
+    resize(sett.value("size").toSize());
+    move(sett.value("pos").toPoint());
 
-    QDir dir;
-    dir.setPath(folder);
-    dir.setFilter(QDir::Files);
-    dir.setNameFilters(nameFilter);
-    QFileInfoList fList = dir.entryInfoList();
+    if (!sett.value("lastPath").toString().isEmpty()){
 
-    int row = 0;
+        folder.setPath(sett.value("lastPath").toString());
+        ui->lineEdit_dir->setText(folder.toNativeSeparators(sett.value("lastPath").toString()));
+        testFolder();
+    }
+}
 
-    for (int i = 0; i < fList.size(); i++){
-        QString fname = fList.at(i).completeBaseName();
+void getBillingNum::selectFolder()
+{
+    QString path = QFileDialog::getExistingDirectory(this, "Выберите папку с файлами...", "HOME", QFileDialog::ShowDirsOnly);
+    folder.setPath(path);
+    ui->lineEdit_dir->setText(folder.toNativeSeparators(path));
 
-        QString region = fname.split("_").at(0);
-        QString type = fname.split("_").at(1);
-        int numCount = 0;
+    testFolder();
+}
 
-        QFile file(fList.at(i).absoluteFilePath());
-        if (file.open(QIODevice::ReadOnly)){
+void getBillingNum::selectFileName()
+{
+    QString _appPath = QApplication::applicationDirPath();
+    QString _fname = QFileDialog::getSaveFileName(this, "Save File", "/home/billingNum.xlsx", "Excel (*.xlsx)");
+    if (!_fname.isEmpty()){
+        get_data(_appPath, _fname);
+        make_message("Файл сохранен!", true);
+    }
+}
 
-            ui->progressBar_num->setValue(0);
+void getBillingNum::get_data(QString appPath, QString fname)
+{
+    report.clear();
+    report.append(QString("<p><b>%0</b>: <span style=\" color:#a60000;\">start</span>").arg(QDateTime::currentDateTime().toString("hh:mm dd.MM.yyyy")));
+    make_report(report);
 
-            QDomDocument *doc = new QDomDocument();
-            doc->setContent(&file);
-            QDomNodeList nodeNumList = doc->elementsByTagName("m1");
+    ui->progressBar_file->setValue(0);
+    ui->progressBar_file->setVisible(true);
+    ui->progressBar_data->setValue(0);
+    ui->progressBar_data->setVisible(true);
 
-            for (int a = 0; a < nodeNumList.count(); a++){
-                QDomElement eNum = nodeNumList.at(a).toElement();
-                QString num = eNum.attribute("u").trimmed();
-                if (!num.isEmpty()){
-                    ui->tableWidget->insertRow(row);
+    QString _temp = QString("%0/temp.xlsx").arg(appPath);
+    QFile _file;
+    if (_file.copy(QDir::toNativeSeparators(_temp), fname)){
+        excel = new QAxObject("Excel.Application");
+        excel->setProperty("Visible", 0);
+        excel->setProperty("DisplayAlerts", 0);
+        wbook = excel->querySubObject("Workbooks");
+        book = wbook->querySubObject("Open (const QString&)", fname);
+        sheets = book->querySubObject("Sheets");
 
-                    QTableWidgetItem *itemNum = new QTableWidgetItem();
-                    itemNum->setText(num);
-                    itemNum->setTextAlignment(Qt::AlignHCenter|Qt::AlignVCenter);
-                    ui->tableWidget->setItem(row, 0, itemNum);
+        QFileInfoList fList = folder.entryInfoList();
+        for (int i = 0; i < fList.size(); i++){
+            QString fSource = fList.at(i).completeBaseName();
 
-                    QTableWidgetItem *itemReg = new QTableWidgetItem();
-                    itemReg->setText(region);
-                    itemReg->setTextAlignment(Qt::AlignLeft|Qt::AlignVCenter);
-                    ui->tableWidget->setItem(row, 1, itemReg);
+            QString region = fSource.split("_").at(0);
+            QString type = fSource.split("_").at(1);
 
-                    QTableWidgetItem *itemType = new QTableWidgetItem();
-                    itemType->setText(type);
-                    itemType->setTextAlignment(Qt::AlignHCenter|Qt::AlignVCenter);
-                    ui->tableWidget->setItem(row, 2, itemType);
-
-                    numCount++;
-                    row++;
+            //проверка наличия нужного листа
+            int countList = sheets->dynamicCall("Count()").toInt();
+            int tList = 0;
+            for (int i = 1; i <= countList; i++){
+                currSheet = sheets->querySubObject("Item(Int)", i);
+                if (currSheet->dynamicCall("Name").toString() == region){
+                    tList = i;                     
+                    break;                   
                 }
-                ui->progressBar_num->setValue(qFloor((a + 1) * 100 / nodeNumList.count()));
-                QApplication::processEvents();
             }
+            if (tList == 0){
+                //добавить лист
+                sheets->dynamicCall("Add()", -4167);
+                currSheet = sheets->querySubObject("Item(Int)", 1);
+                currSheet->dynamicCall("Name", region);
+                //внести данные в 1-2 колонку
+                QFile file(fList.at(i).absoluteFilePath());
+                if (file.open(QIODevice::ReadOnly)){
 
-            file.close();
-            qDebug() << region << " - " << type << ": " << numCount;
+                    QDomDocument *doc = new QDomDocument();
+                    doc->setContent(&file);
+                    QDomNodeList nodeNumList = doc->elementsByTagName("m1");
+
+                    //header
+                    QAxObject *hNum = currSheet->querySubObject("Cells(Int, Int)", 1, 1);
+                    hNum->dynamicCall("Value", "Номер");
+                    QAxObject *hType = currSheet->querySubObject("Cells(Int, Int)", 1, 2);
+                    hType->dynamicCall("Value", "Тип");
+
+                    for (int a = 0; a < nodeNumList.count(); a++){
+                        QDomElement eNum = nodeNumList.at(a).toElement();
+                        QString num = eNum.attribute("u").trimmed();
+
+                        if (!num.isEmpty()){
+                            //header
+                            QAxObject *dNum = currSheet->querySubObject("Cells(Int, Int)", a + 2, 1);
+                            dNum->dynamicCall("Value", num);
+                            dNum->dynamicCall("WrapText", 0);
+                            QAxObject *dType = currSheet->querySubObject("Cells(Int, Int)", a + 2, 2);
+                            dType->dynamicCall("Value", type);
+                            dType->dynamicCall("WrapText", 0);
+                        }
+
+                        ui->progressBar_data->setValue(qFloor((a + 1) * 100 / nodeNumList.count()));
+                        QApplication::processEvents();
+                    }
+                    //auto_fit
+                    QAxObject *aColumns = currSheet->querySubObject("Range(QVariant)", "A1:T5000");
+                    aColumns->querySubObject("EntireColumn")->dynamicCall("AutoFit");
+                    file.close();
+
+                    report.append(QString("<br /><b>%0</b>: <span style=\" color:#006300;\">%1 - %2</span>").arg(QDateTime::currentDateTime().toString("hh:mm dd.MM.yyyy"))
+                                  .arg(fList.at(i).completeBaseName())
+                                  .arg(nodeNumList.count()));
+                    make_report(report);
+                } else {
+                    qDebug() << "file don't open";
+                }
+
+            } else if (tList > 0){
+                currSheet = sheets->querySubObject("Item(Int)", tList);
+                // проверить наличие свободных колонок
+                int col = 0;
+                for (int i = 4; i < 21; i = i + 3){
+                    QAxObject *test = currSheet->querySubObject("Cells(Int, Int)", 1, i);
+                    QString testStr = test->dynamicCall("Value").toString();
+                    if (testStr.isEmpty()){
+                        col = i;
+                        break;
+                    }
+                }
+                QFile file(fList.at(i).absoluteFilePath());
+                if (file.open(QIODevice::ReadOnly)){
+
+                    ui->progressBar_data->setValue(0);
+
+                    QDomDocument *doc = new QDomDocument();
+                    doc->setContent(&file);
+                    QDomNodeList nodeNumList = doc->elementsByTagName("m1");
+
+                    //header
+                    QAxObject *hNum = currSheet->querySubObject("Cells(Int, Int)", 1, col);
+                    hNum->dynamicCall("Value", "Номер");
+                    QAxObject *hType = currSheet->querySubObject("Cells(Int, Int)", 1, col + 1);
+                    hType->dynamicCall("Value", "Тип");
+
+                    for (int a = 0; a < nodeNumList.count(); a++){
+                        QDomElement eNum = nodeNumList.at(a).toElement();
+                        QString num = eNum.attribute("u").trimmed();
+
+                        if (!num.isEmpty()){
+                            //header
+                            QAxObject *dNum = currSheet->querySubObject("Cells(Int, Int)", a + 2, col);
+                            dNum->dynamicCall("Value", num);
+                            dNum->dynamicCall("WrapText", 0);
+                            QAxObject *dType = currSheet->querySubObject("Cells(Int, Int)", a + 2, col + 1);
+                            dType->dynamicCall("Value", type);
+                            dType->dynamicCall("WrapText", 0);
+                        }
+
+                        ui->progressBar_data->setValue(qFloor((a + 1) * 100 / nodeNumList.count()));
+                        QApplication::processEvents();
+                    }
+                    //auto_fit
+                    QAxObject *aColumns = currSheet->querySubObject("Range(QVariant)", "A1:T5000");
+                    //aColumns->dynamicCall("WrapText(Bool)", false);
+                    aColumns->querySubObject("EntireColumn")->dynamicCall("AutoFit");
+                    file.close();
+
+                    report.append(QString("<br /><b>%0</b>: <span style=\" color:#006300;\">%1 - %2</span>")
+                                  .arg(QDateTime::currentDateTime().toString("hh:mm dd.MM.yyyy"))
+                                  .arg(fList.at(i).completeBaseName())
+                                  .arg(nodeNumList.count()));
+                    make_report(report);
+                }else {
+                    qDebug() << "file don't open";
+                }
+
+            }
+            ui->progressBar_file->setValue(qFloor((i + 1) * 100 / fList.size()));
+            QApplication::processEvents();
         }
-        ui->progressBar->setValue(qFloor((i + 1) * 100 / fList.count()));
-        QApplication::processEvents();
-    }
-    ui->tableWidget->resizeColumnsToContents();
-    ui->tableWidget->horizontalHeader()->setStretchLastSection(true);
 
-    ui->progressBar->hide();
-    ui->progressBar_num->hide();
+        book->dynamicCall("Save()");
+        wbook->dynamicCall("Close()");
+        excel->dynamicCall("Quit()");
+        delete excel;
+    }
+
+    report.append(QString("<br /><b>%0</b>: <span style=\" color:#a60000;\">finish</span></p>").arg(QDateTime::currentDateTime().toString("hh:mm dd.MM.yyyy")));
+    make_report(report);
+    ui->progressBar_file->hide();
+    ui->progressBar_data->hide();
+}
+
+void getBillingNum::testFolder()
+{
+    if (folder.isReadable()){
+        QStringList nameFilter;
+        nameFilter << "*.fp3";
+        folder.setNameFilters(nameFilter);
+        folder.setFilter(QDir::Files);
+
+        int countFiles = folder.entryInfoList().size();
+        if (countFiles == 0){
+            make_message("Файлы не найдены", false);
+            ui->pushButton_getData->setEnabled(false);
+        } else {
+            make_message(QString("Найдено файлов: %0").arg(countFiles), true);
+            ui->pushButton_getData->setEnabled(true);
+        }
+    } else {
+        make_message("Файлы не найдены", false);
+        ui->pushButton_getData->setEnabled(false);
+    }
+}
+
+void getBillingNum::make_message(QString str, bool v)
+{
+    ui->groupBox_message->setVisible(true);
+    int delay = 0;
+    if (v){
+        ui->groupBox_message->setStyleSheet("background-color: #006300; border-radius: 9px;");
+        delay = 5000;
+    } else {
+        ui->groupBox_message->setStyleSheet("background-color: #a60000; border-radius: 9px;");
+        delay = 15000;
+    }
+    ui->l_messa->setText(str);
+    ui->l_messa->setStyleSheet("color: #FFF5EE; font-weight: bold; ");
+    timer->start(delay);
+}
+
+void getBillingNum::make_report(QString str)
+{
+    ui->textEdit_report->clear();
+    ui->textEdit_report->setHtml(str);
 }
--- getBillingNum/getbillingnum.h
+++ getBillingNum/getbillingnum.h
@@ -3,6 +3,13 @@
 
 #include <QMainWindow>
 #include <QtXml/QtXml>
+#include <QFileDialog>
+#include <QTimer>
+#include <QSettings>
+#include <QCloseEvent>
+
+#include <ActiveQt/ActiveQt>
+
 
 namespace Ui {
 class getBillingNum;
@@ -16,14 +23,36 @@ public:
     explicit getBillingNum(QWidget *parent = 0);
     ~getBillingNum();
 
+    QTimer *timer;
+    QDir folder;
 
-    QString folder;
+    QAxObject *excel;
+    QAxObject *wbook;
+    QAxObject *book;
+    QAxObject *sheets;
+    QAxObject *currSheet;
+
+    QString report;
+
+    virtual void closeEvent(QCloseEvent *event);
 
 public slots:
-    void get_data();
+    void writeSetting();
+    void readSetting();
+
+    void selectFolder();
+    void selectFileName();
+
+    void get_data(QString appPath, QString fname);
+    void testFolder();
+
+    void make_message(QString str, bool v);
+    void make_report(QString str);
 
 private:
     Ui::getBillingNum *ui;
 };
+
+
 
 #endif // GETBILLINGNUM_H
